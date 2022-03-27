9239460f640cf355b522ae1812623e910ebacf30
machadoreisdaniel <70669140+machadoreisdaniel@users.noreply.github.com>
2022-03-27 17:24:10 -0300

    Fix UnitTest

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Daniel Reis 2020"
+    return "Hello World - Daniel Reis"
 
 if __name__ == '__main__':
     app.run()
